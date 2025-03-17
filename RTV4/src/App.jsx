d8695bf01e3170577d778bc22e60232d4ae50524
hadil <hadylkhelif18@gmail.com>
2025-03-17 21:27:15 +0100

    Updated styles and modified App.js

--- RTV4/src/App.jsx
+++ RTV4/src/App.jsx
@@ -5,8 +5,9 @@ const App = () => {
     <div className='bg-deepblue container'>
       <h1 className='text-5xl font-bold text-white'>Hello, Tailwind CSS!</h1>
       <button className='bg-amber text-purple-500 border-4 text-2xl font-bold mt-12 px-6 py-2 rounded '>Click me!</button>
+      <input type='texte' placeholder='Enter your name' className='bg-white text-lg text-amber boder-2 p-4 rounded ml-4 outline-0' />
     </div>
-  )
+  ) 
 }
 
 export default App
