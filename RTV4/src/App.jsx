d3834e533941aea323a6dd8dd88c04266eeb9b6a
hadil <hadylkhelif18@gmail.com>
2025-03-17 21:15:59 +0100

    Initial commit

---
+++ RTV4/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-deepblue container'>
+      <h1 className='text-5xl font-bold text-white'>Hello, Tailwind CSS!</h1>
+      <button className='bg-amber text-purple-500 border-4 text-2xl font-bold mt-12 px-6 py-2 rounded '>Click me!</button>
+    </div>
+  )
+}
+
+export default App
